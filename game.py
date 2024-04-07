c550327333cf3576d62d92c2e287e6fc41f1e772
BobbyPeanut1 <jonathan.zamanski@gmail.com>
2024-04-07 19:54:27 +0300

    Added the game

--- game.py
+++ game.py
@@ -1,1 +1,12 @@
+import random
 print("Welcome to the game!")
+
+print(" Welcome to the game! \n call Heads or Tails: ")
+
+player_input = input()
+coin_flip = random.choice(["Heads", "Tails"])
+
+if player_input == coin_flip:
+    print("You won!")
+else:
+    print(f"Sorry it was {coin_flip}")
