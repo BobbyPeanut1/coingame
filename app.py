efa91fdd8473c9d02777133491a5fd1587582ef9
BobbyPeanut1 <jonathan.zamanski@gmail.com>
2024-04-07 19:11:57 +0300

    Added multiplication

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ def add_two_numbers(a, b):
 def subtract_two_numbers(a, b):
     return f"{a} + {b} = {a - b}"
 
+def multiply_two_numbers(a, b):
+    return f"{a} + {b} = {a * b}"
+
 x = int(input("Enter a number: "))
 y = int(input("Enter a number: "))
-print(subtract_two_numbers(x, y))
+print(subtract_two_numbers(x, y))  
